8572eeceeaf10a8e6445753cba44c2aecfd1dda8
TigranAghajanyan <123482588+TigranAghajanyan@users.noreply.github.com>
2023-11-10 00:03:13 +0400

    Update main.py

--- lec_10/main.py
+++ lec_10/main.py
@@ -1,6 +1,6 @@
-from vehicle import my_vehicle
+from vehicle import my_vehicle as Vehicle
 
-class Car(my_vehicle):
+class Car(Vehicle):
     def __init__(self, model, year, color):
         super().__init__(model)
         self.year = year
@@ -9,7 +9,18 @@ class Car(my_vehicle):
         super().Information()
         print('Year is: ',self.year)
         print('Color is: ',self.color)
+        
+class Plane(Vehicle):
+    def __init__(self, model, year, color):
+        super().__init__( model)
 
+class Boat(Vehicle):
+    def __init__(self, make, model, year):
+        super().__init__( model)
+
+class RaceCar(Car):
+    def __init__(self, color, model, year):
+        super().__init__(color, model, year)
 
 car = Car('BMV', 2005, 'black')
 car.information()

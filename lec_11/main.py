76b0c7dcfa771b639dccd7f39f9c420d82d6658c
TigranAghajanyan <123482588+TigranAghajanyan@users.noreply.github.com>
2023-11-17 00:09:08 +0400

    Update main.py

--- lec_11/main.py
+++ lec_11/main.py
@@ -5,7 +5,7 @@ posts = requests.get(f'{base_url}/posts').json()
 filtered_titles = [{'userId':post_dict['userId'],'id':post_dict["id"]} for post_dict in posts if len(post_dict['title'].split()) <= 6]
 
 # print(posts)
-print("---------Titles with 6 or fewer words---------")
+print("---------Ids where titles with 6 or fewer words---------")
 print(filtered_titles)
 
 
@@ -15,8 +15,7 @@ filtered_bodies = [post_dict for post_dict in posts if len(post_dict['body'].spl
 print("\nPosts with 3 or fewer lines in the body:")
 for post in filtered_bodies:
     print(f"Title: {post['title']}\nBody:\n{post['body']}\n")
-
-
+print(len(filtered_bodies), ' posts')
 
 
 
@@ -26,7 +25,7 @@ new_post = {
     'body': 'New post body content'
 }
 created_post = requests.post(f'{base_url}/posts', json=new_post).json()
-print("\nCreated Post:")
+
 
 post_to_update_id = 1
 updated_post = {
@@ -42,4 +41,4 @@ requests.delete(f'{base_url}/posts/{post_to_delete_id}')
 
 # Confirm deletion by trying to fetch the deleted post
 deleted_post = requests.get(f'{base_url}/posts/{post_to_delete_id}')
-print("deleted_post status_code: ", deleted_post.status_code)  # Should print 404
+print("\ndeleted_post status_code: ", deleted_post.status_code)  # Should print 404
